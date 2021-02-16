161a75adf4bcffba2e043d37eebb07f9467cf025
Martyna Puzio <martyna.puzio@beclose.eu>
2021-02-16 14:29:21 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+ctx.fillStyle = "green";
+ctx.fillRect(100, 50,150,75); // fillRect(x,y,width,height)
+// rysownaie linii
